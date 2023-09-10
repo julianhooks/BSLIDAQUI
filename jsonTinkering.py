530a7563afd7f774576c72fa07286e87356c7acb
Julian Hooks <julian.delb.hooks@gmail.com>
2023-09-10 17:56:00 -0400

    Deleted tinkering files

--- jsonTinkering.py
+++
@@ -1,8 +0,0 @@
-import datetime
-
-def main():
-    f = open(str(datetime.datetime.today().strftime("%S_%M_%H_%d_%m_%y"))+".log","x")
-    f.close()
-
-if (__name__ == "__main__"):
-    main()
--- matplotlibTinkering.py
+++
@@ -1,9 +0,0 @@
-import matplotlib.pyplot as plt
-import matplotlib.animation as animation
-import numpy as np
-
-def main():
-    pass
-
-if (__name__ == "__main__"):
-    main()
