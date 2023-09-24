af232d37aaa9bc9328569708e71e5f2cd035a2d9
Julian Hooks <julian.delb.hooks@gmail.com>
2023-09-24 14:32:13 -0400

    Working on Graphs

--- Instrument.py
+++ Instrument.py
@@ -1,5 +1,41 @@
 import tkinter as tk
+import numpy as np
 from labjack import ljm
+
+from matplotlib.backends.backend_tkagg import (
+    FigureCanvasTkAgg, NavigationToolbar2Tk)
+from matplotlib.figure import Figure
+
+
+class Graph(tk.Widget):
+    def __init__(self, inputVariable: str, maxLength: int) -> None:
+        self.tkVariable = inputVariable
+        self.data = []
+        self.length = maxLength
+        #do graph setup
+
+        fig = Figure(figsize=(5, 4), dpi=100)
+        t = np.arange(0, 3, .01)
+        ax = fig.add_subplot()
+        line = ax.plot(t, 2 * np.sin(2 * np.pi * t))
+        ax.set_xlabel("time [s]")
+        ax.set_ylabel("f(t)")
+
+        canvas = FigureCanvasTkAgg(fig, master=root) 
+
+
+    def updateGraph(self) -> None:
+        if (len(self.data) == self.length):
+            trash = self.data.pop(0)
+            self.data.append(float(tk.Frame.getvar(self.tkVariable)))
+            return
+        elif (len(self.data) <= self.length):
+            self.data.append(float(tk.Frame.getvar(self.tkVariable)))
+            return
+        else:
+            while(len(self.data) > self.length):
+                trash = self.data.pop(0)
+            return
 
 def loadInstrument(parentFrame: tk.Frame, label: str, units: str, rowParam: int, columnParam: int, styleDict: dict) -> None:
     widgetFrame = tk.Frame(parentFrame)
@@ -11,17 +47,34 @@ def loadInstrument(parentFrame: tk.Frame, label: str, units: str, rowParam: int,
     widgetFrame.grid(column=columnParam,row=rowParam)
     #widgetFrame.config(background="white")
 
+def loadGraph(parentFrame: tk.Frame, label: str, units: str, rowParam: int, columnParam: int, styleDict: dict) -> None:
+    widgetFrame = tk.Frame(parentFrame)
+    widgetFrame.grid(sticky=(tk.N,tk.S,tk.E,tk.W))
+    widgetFrame.config(padx=styleDict["padding"],pady=styleDict["padding"])
+    tk.Label(widgetFrame, text = label, font = (styleDict["labelFont"],styleDict["labelSize"],"normal"), fg=styleDict["labelColor"]).grid(column=0,row=0,columnspan=2)
+    #create and pack a graph into this frame
+    widgetFrame.grid(column=columnParam,row=rowParam)
+    pass
 
 def loadDataVars(masterFrame: tk.Frame, instrumentConfigDict: dict) -> None:
     for i in instrumentConfigDict:
         tk.StringVar(masterFrame, i["label"])
 
-def updateDataVars(masterFrame: tk.Frame, instrumentConfigDict: dict, labjackHandle: int) -> None:
-    for i in instrumentConfigDict:
-        voltageData = ljm.eReadName(labjackHandle,i["pin"])
-        scaledData = voltageData*i["scalingFactor"] + i["offset"]
-        masterFrame.setvar(name=i["label"],value=str(scaledData)[0:10])
-
 def loadInstruments(mainFrame: tk.Frame, instrumentConfigDict: dict, styleDict: dict) -> None:
     for i in instrumentConfigDict:
         loadInstrument(mainFrame,i["label"],i["unit"],i["row"],i["column"],styleDict)
+
+def updateGraph(masterFrame: tk.Frame, graph: dict, labjackHandle: int) -> None:
+    pass
+
+def updateInstrument(masterFrame: tk.Frame, instrument: dict, labjackHandle: int) -> None:
+    voltageData = ljm.eReadName(labjackHandle,instrument["pin"])
+    scaledData = voltageData*instrument["scalingFactor"] + instrument["offset"]
+    masterFrame.setvar(name=instrument["label"],value=str(scaledData)[0:10])
+
+def updateDataVars(masterFrame: tk.Frame, instrumentConfigDict: dict, labjackHandle: int) -> None:
+    for i in instrumentConfigDict:
+        if (i["type"] == "Number"):
+            updateInstrument(masterFrame, i, labjackHandle)
+        elif (i["type"] == "Graph"):
+            updateGraph(masterFrame, i, labjackHandle)
