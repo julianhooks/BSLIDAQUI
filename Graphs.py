f60fe004d9ad6e611f19862fa1a59972e9d9aa5c
Julian Hooks <julian.delb.hooks@gmail.com>
2023-09-30 12:47:01 -0400

    Added working graphs

--- Graphs.py
+++ Graphs.py
@@ -1,15 +1,16 @@
 import numpy as np
 import tkinter as tk
 import multiprocessing
-
-import matplotlib.animation
 import matplotlib.figure
+import matplotlib.animation
 from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg
 
+ani = []
 
 def updateGraph(graph: dict, voltageData: multiprocessing.Array) -> None:
     trash = graph["values"].pop()
-    graph["values"].append_left(voltageData[graph["index"]])
+    graph["values"].appendleft(voltageData[graph["index"]] * graph["scalingFactor"] + graph["offset"])
+    #print(graph["values"])
 
 def loadGraph(parentFrame: tk.Frame, graph: dict, styleDict: dict) -> None:
     #setting up the tk frame
@@ -19,10 +20,10 @@ def loadGraph(parentFrame: tk.Frame, graph: dict, styleDict: dict) -> None:
     tk.Label(widgetFrame, text = graph["label"], font = (styleDict["labelFont"],styleDict["labelSize"],"normal"), fg=styleDict["labelColor"]).grid(column=0,row=0,columnspan=2)
 
     #setting up the matplot plot
-    fig = matplotlib.figure.Figure(figsize=(5, 4), dpi=100)
-    t = np.arange(0, graph["range"]*graph["interval"], graph["interval"]) #replace max with i.range / i.interval
+    fig = matplotlib.figure.Figure(figsize=(3, 3), dpi=100)
+    t = np.arange(0, graph["range"], graph["interval"]) #replace max with i.range / i.interval
     ax = fig.add_subplot()
-    line, = ax.plot(t, 0)
+    line, = ax.plot(t, graph["values"])
 
     #define the animation function, since it's within this function we can use variables in our upper scope
     def animate(i):
@@ -35,8 +36,16 @@ def loadGraph(parentFrame: tk.Frame, graph: dict, styleDict: dict) -> None:
     canvas = FigureCanvasTkAgg(fig, master=widgetFrame)  # A tk.DrawingArea.
     canvas.draw()
 
-    canvas.get_tk_widget().pack(side=tk.TOP, fill=tk.BOTH, expand=True)
+    canvas.get_tk_widget().grid(row=1,column=0)
 
-    ani = matplotlib.animation.FuncAnimation(fig, animate, interval=20, blit=True, save_count=50)
 
-    widgetFrame.grid(column=graph["columnParam"],row=graph["rowParam"])
+    #The FuncAnimation used for each graph must last as long as the animation, 
+    #so we have to delare a global variable to avoid the function being destroyed
+    #when the loadGraph function ends. By appending to a list instead of setting 
+    #a unique variable, we allow for multiple independent graphs.
+
+    global ani
+
+    ani.append(matplotlib.animation.FuncAnimation(fig, animate, interval=20, blit=True, save_count=50))
+
+    widgetFrame.grid(column=graph["column"],row=graph["row"])
--- InterfaceUI.py
+++ InterfaceUI.py
@@ -7,15 +7,25 @@ import Instrument
 
 isWindowOpen = True
 
-def loadWidgets(mainFrame: tk.Frame, instrumentConfigDict: dict, styleDict: dict, isLoggingQueue: multiprocessing.queue) -> None:
+def loadWidgets(mainFrame: tk.Frame, instrumentConfigDict: dict, styleDict: dict, isLogging: multiprocessing.Value) -> None:
     for i in instrumentConfigDict:
-        if (i["type"] == "number"):
+        if (i["type"] == "Number"):
             Instrument.loadInstrument(mainFrame,i,styleDict)
-        elif (i["type"] == "graph"):
+        elif (i["type"] == "Graph"):
             Graphs.loadGraph(mainFrame,i,styleDict)
+
+    Instrument.loadDataVars(mainFrame,instrumentConfigDict)
+
     # LOAD LOGGING BUTTONS
-        tk.Button(mainFrame, text="Start Data Logging", command=isLoggingQueue.queue(1)).grid(row=3,column=0)
-        tk.Button(mainFrame, text="End Data Loging", command =isLoggingQueue.queue(0)).grid(row=3,column=1)
+
+    def startLog():
+        isLogging.value = 1
+
+    def stopLog():
+        isLogging.value = 0
+
+    tk.Button(mainFrame, text="Start Data Logging", command = startLog).grid(row=3,column=0)
+    tk.Button(mainFrame, text="End Data Loging", command = stopLog).grid(row=3,column=1)
 
 def updateWidgets(masterFrame: tk.Frame, instrumentConfigDict: dict, voltageData: multiprocessing.Array) -> None:
     for i in instrumentConfigDict:
@@ -24,31 +34,37 @@ def updateWidgets(masterFrame: tk.Frame, instrumentConfigDict: dict, voltageData
         elif (i["type"] == "Graph"):
             Graphs.updateGraph(i, voltageData)
 
-def UILoop(voltageData: multiprocessing.Array, instrumentConfigData: dict, isWindowOpenGlobal: multiprocessing.Value, isLogging: multiprocessing.Value, windowStyle: dict, instrumentStyle: dict) -> None:
+def UILoop(voltageData: multiprocessing.Array, instrumentConfigData: dict, 
+           isWindowOpenGlobal: multiprocessing.Value, isLogging: 
+           multiprocessing.Value, windowStyle: dict, instrumentStyle: dict) -> None:
     
     root = tk.Tk()
+
+    def onClosing():
+        isWindowOpenGlobal.value = 0
 
     root.protocol("WM_DELETE_WINDOW", onClosing)
 
     mainFrame = tk.Frame(root)
+
     mainFrame.grid()
     mainFrame.config(bg=windowStyle["backgroundColor"])
     mainFrame.config(padx=windowStyle["padding"],pady=windowStyle["padding"])
     mainFrame.grid_columnconfigure(tk.ALL,weight=1,pad=instrumentStyle["margin"],minsize=instrumentStyle["minWidth"])
     mainFrame.grid_rowconfigure(tk.ALL,weight=1,pad=instrumentStyle["margin"],minsize=instrumentStyle["minHeight"])
 
-    #Add logging buttons
+    loadWidgets(mainFrame,instrumentConfigData,instrumentStyle, isLogging)
 
-    loadWidgets(mainFrame,instrumentConfigData,instrumentStyle)
-    Instrument.loadDataVars(mainFrame,instrumentConfigData)
-
-    while(isWindowOpen):
-        startTime = time.time()
-        updateWidgets()
+    while(isWindowOpenGlobal.value):
         root.update()
         root.update_idletasks()
+        startTime = time.time()
+        updateWidgets(mainFrame,instrumentConfigData,voltageData)
         #Add an update buffer to smooth graphs?
         while(time.time()-startTime < 0.04): #25 FPS lock
             pass
     
-    isWindowOpenGlobal.value = int(isWindowOpen)
+    isWindowOpenGlobal.value = 0
+    root.destroy()
+
+    exit()
