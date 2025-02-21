ee3937e1ef5973a06a7e247497b572fc165899f7
julianhooks <julian.delb.hooks@gmail.com>
2025-02-20 22:46:58 -0500

    Added closing functions, format changes

--- DAQCommunications.py
+++ DAQCommunications.py
@@ -1,42 +1,30 @@
 import logging
 import multiprocessing
+import time
+
 import serial
 import serial.serialutil
 import serial.tools.list_ports
-import time
 from labjack import ljm
 
 def main():
-    handle = LoadSerial()
+    handle = loadSerial()
     print(handle)
     while(True):
         print("Testing...")
         print(handle.write(b'RD000\n'))
         print(handle.readline()[:-2])
         time.sleep(1)
-        
-#Get voltages from labjack
-def GetVoltagesUSB(voltageData: multiprocessing.Array, instrumentConfigData: dict, serialHandle: serial.Serial):
-    for i in instrumentConfigData:
-        # Skip for non-voltage inputs
-        if (i["pin"] == None):
-            continue
-        # Read voltage
-        # Make read request
-        serialHandle.write(b'RD' + i["pin"] + b'\n')
-        # Calibrate
-        voltageData[i["index"]] = 5.0*(int(serialHandle.readline()[:-2])/1024)
 
-
-def GetVoltagesLabjack(voltageData: multiprocessing.Array, instrumentConfigData: dict, labjackHandle: int) -> None:
+def getVoltagesLabjack(voltageData: multiprocessing.Array, instrumentConfigData: dict, labjackHandle: int) -> None:
     for i in instrumentConfigData:
         try: 
             voltageData[i["index"]] = ljm.eReadName(labjackHandle, i["pin"])
         except KeyError:
-            #logging.debug(f"{i["label"]} has no assigned pin.")
+            logging.debug(f"{i['label']} has no assigned pin.")
             pass
         except ljm.LJME_INVALID_NAME:
-            #logging.debug(f"{i["label"]} has no assigned pin.")
+            logging.debug(f"{i['label']} has no assigned pin.")
             if (i["pin"] == None):
                 pass
             else:
@@ -46,15 +34,37 @@ def GetVoltagesLabjack(voltageData: multiprocessing.Array, instrumentConfigData:
         except UnboundLocalError:
             raise UnboundLocalError
         
-def LoadLabJack() -> int:
+def loadLabJack() -> int:
     #Attempt to connect to labjack
     try: 
         handle = ljm.openS("ANY","ANY","ANY")
     except ljm.LJMError:
         logging.critical("Could not connect to Labjack.")
+        handle = -1
     return handle
 
-def LoadSerial() -> serial.Serial:
+def closeLabJack(handle:int) -> None:
+    #Clean up labjack connection
+    try:
+        ljm.close(handle)
+    except ljm.LJMError:
+        logging.error("Closing labjack connection failed")
+    except UnboundLocalError:
+        logging.error("Labjack not connected at program termination")
+        pass #Case where there is no connection to disconnect
+
+def getVoltagesUSB(voltageData: multiprocessing.Array, instrumentConfigData: dict, serialHandle: serial.Serial):
+    for i in instrumentConfigData:
+        # Skip for  -voltage inputs
+        if (i["pin"] == None):
+            continue
+        # Read voltage
+        # Make read request
+        serialHandle.write(b'RD' + i["pin"] + b'\n')
+        # Calibrate
+        voltageData[i["index"]] = ((5.0*int(serialHandle.readline()[:-2]))/1024)
+
+def loadSerial() -> serial.Serial:
     handle = serial.Serial()
     handle.baudrate = 115200
     handle.timeout = 1
@@ -80,10 +90,12 @@ def LoadSerial() -> serial.Serial:
         else:
             logging.info(f"Successfully established connection on port {port}.")
             return handle
-        
     if (not handle.is_open):
         raise serial.SerialException
     return None
+
+def closeSerial(handle: serial.Serial) -> None:    
+    handle.close()
         
 if (__name__=="__main__"):
     main()
